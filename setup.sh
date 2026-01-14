1c4f14ea215fd2deae7896f35f7438837019c89c
NaimKabir <kabir.naim@gmail.com>
2026-01-14 01:34:51 -0800

    update setup to setup preferences

--- setup.sh
+++ setup.sh
@@ -9,11 +9,13 @@ REPO_PATH="$1"
 TARGET_TMUX_CONF=~/.tmux.conf
 TARGET_VIM_FOLDER=~/.vim
 TARGET_VIMRC=~/.vimrc
+TARGET_AEROSPACE=~/.aerospace.toml
 
 # Cleanup
 rm -f $TARGET_TMUX_CONF
 rm -rf $TARGET_VIM_FOLDER
 rm -f $TARGET_VIMRC
+rm -f $TARGET_AEROSPACE
 
 # Creating dirs
 
@@ -24,6 +26,7 @@ mkdir -p ~/vimtmp # creating a place for vim to store backups/swaps/undos
 ln -s $REPO_PATH/tmux.conf $TARGET_TMUX_CONF
 ln -s $REPO_PATH/vimrc $TARGET_VIMRC
 ln -s $REPO_PATH/vim $TARGET_VIM_FOLDER 
+ln -s $REPO_PATH/aerospace.toml $TARGET_VIM_FOLDER 
 
 # Clone git submodules to get plugin source
 git submodule update --init --recursive
